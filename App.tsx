de3e544ee6eb22250416d95abde84c869fd7626c
Mickaël Mesloub <m.mesloub@beapp.fr>
2023-10-09 21:28:58 +0200

    edit(App) changed safe area view

--- App.tsx
+++ App.tsx
@@ -2,11 +2,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PaperProvider } from 'react-native-paper';
 import AppNavigation from './src/navigation/AppNavigation';
-import SafeViewAndroid from './src/components/SafeViewAndroid';
 
 const App = () => {
   return (
-    <SafeAreaProvider style={SafeViewAndroid.AndroidSafeArea}>
+    <SafeAreaProvider>
       <PaperProvider>
         <NavigationContainer>
           <AppNavigation />
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useEffect, useState } from 'react';
 import DailyPicture from '../components/DailyPicture';
 
